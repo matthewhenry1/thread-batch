d77ab5de566320ac48707fed136c05da743c4d19
Matthew Henry <matthewhenry@slack-corp.com>
2021-03-31 12:38:02 -0400

    update

--- batch.js
+++ batch.js
@@ -1,22 +1,22 @@
 // calc num queries
-total = 9999
-page_size = 100
-num_queries = Math.ceil(total/page_size)
+const total = 9999
+const page_size = 100
+const num_queries = Math.ceil(total/page_size)
 console.log('num_queries: ' + num_queries)
 
 // calc pages
-pages = []
+const pages = []
 for (let i = 0; i < num_queries; i++) {
     pages.push(i*page_size)
 }
 console.log(pages)
 
 // determine batch size
-batch_size = 2
+const batch_size = 2
 console.log('batch_size: ' + batch_size)
 
 for (let n = 0; n < num_queries; n++) {
-    slice = pages.slice(n * batch_size, (n + 1) * batch_size)
+    let slice = pages.slice(n * batch_size, (n + 1) * batch_size)
     if (slice.length > 0){
         console.log(n+'_slice: ' + slice)
     } else {
